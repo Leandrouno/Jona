ab3decd14d4dab00dbf772b0e10d6d80abf32c03
Leandrouno <lmugnolo@gmail.com>
2024-05-01 19:13:27 -0300

    Update uln2003.ino

--- uln2003.ino
+++ uln2003.ino
@@ -1,51 +1,110 @@
-#include <Arduino.h>
 
-// Definición de pines del motor paso a paso
-const int IN1 = D0;
-const int IN2 = D1;
-const int IN3 = D2;
-const int IN4 = D3;
+/*
+ * cheapStepper_newMoveTo.ino
+ * ///////////////////////////////////////////
+ * using CheapStepper Arduino library v.0.2.0
+ * created by Tyler Henry, 7/2016
+ * ///////////////////////////////////////////
+ * 
+ * This sketch illustrates the library's
+ * "non-blocking" move functions -
+ * i.e. you can perform moves with the stepper over time
+ * while still running other code in your loop()
+ * 
+ * This can be useful if your Arduino is multi-tasking,
+ * but be careful: if the other code in your loop()
+ * slows down your Arduino, the stepper motor may
+ * slow down or move with a stutter
+ * 
+ * //////////////////////////////////////////////////////
+ */
 
-// Definición de secuencia de pasos
-int seq[4][4] = {
-  {1,0,0,1},
-  {0,1,0,1},
-  {0,0,1,1},
-  {1,1,0,0}
-};
+// first, include the library :)
 
-// Definición de la velocidad del motor (pasos por segundo)
-const int velocidad = 80;
+#include <CheapStepper.h>
 
-// Variable para almacenar la posición actual del motor
-int step = 0;
+// next, declare the stepper
+// and connect pins 8,9,10,11 to IN1,IN2,IN3,IN4 on ULN2003 board
+
+CheapStepper stepper (D0,D1,D2,D3) ;
+
+
+ // let's also create a boolean variable to save the direction of our rotation
+ // and a timer variable to keep track of move times
+
+bool moveClockwise = true;
+unsigned long moveStartTime = 0; // this will save the time (millis()) when we started each new move
+
 
 void setup() {
-  // Configuración de pines como salida
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+
+  // let's run the stepper at 12rpm (if using 5V power) - the default is ~16 rpm
+
+  stepper.setRpm(12);
+
+  // let's print out the RPM to make sure the setting worked
+  
+  Serial.begin(9600);
+  Serial.print("stepper RPM: "); Serial.print(stepper.getRpm());
+  Serial.println();
+
+  // and let's print the delay time (in microseconds) between each step
+  // the delay is based on the RPM setting:
+  // it's how long the stepper will wait before each step
+
+  Serial.print("stepper delay (micros): "); Serial.print(stepper.getDelay());
+  Serial.println(); Serial.println();
+
+  // now let's set up our first move...
+  // let's move a half rotation from the start point
+
+  stepper.newMoveTo(moveClockwise, 2048);
+  /* this is the same as: 
+   * stepper.newMoveToDegree(clockwise, 180);
+   * because there are 4096 (default) steps in a full rotation
+   */
+  moveStartTime = millis(); // let's save the time at which we started this move
+  
 }
 
 void loop() {
-  // Ciclo para recorrer la secuencia de pasos
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      // Activar la bobina correspondiente
-      digitalWrite(IN1, seq[i][0]);
-      digitalWrite(IN2, seq[i][1]);
-      digitalWrite(IN3, seq[i][2]);
-      digitalWrite(IN4, seq[i][3]);
 
-      // Retardo para la velocidad del motor
-      delay(1000 / velocidad);
+  // we need to call run() during loop() 
+  // in order to keep the stepper moving
+  // if we are using non-blocking moves
+  
+  stepper.run();
 
-      // Cambio de estado para la siguiente bobina
-      step++;
-      if (step >= 16) {
-        step = 0;
-      }
-    }
+  ////////////////////////////////
+  // now the stepper is moving, //
+  // let's do some other stuff! //
+  ////////////////////////////////
+
+  // let's check how many steps are left in the current move:
+  
+  int stepsLeft = stepper.getStepsLeft();
+
+  // if the current move is done...
+  
+  if (stepsLeft == 0){
+
+    // let's print the position of the stepper to serial
+    
+    Serial.print("stepper position: "); Serial.print(stepper.getStep());
+    Serial.println();
+
+    // and now let's print the time the move took
+
+    unsigned long timeTook = millis() - moveStartTime; // calculate time elapsed since move start
+    Serial.print("move took (ms): "); Serial.print(timeTook);
+    Serial.println(); Serial.println();
+    
+    // let's start a new move in the reverse direction
+    
+    moveClockwise = !moveClockwise; // reverse direction
+    stepper.newMoveDegrees (moveClockwise, 180); // move 180 degrees from current position
+    moveStartTime = millis(); // reset move start time
+
   }
+
 }
